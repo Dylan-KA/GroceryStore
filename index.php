415202d8e0aca4412ce9004f900473294e0759b7
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2024-04-01 17:48:04 +1100

    Cart launches checkout

--- index.php
+++ index.php
@@ -19,8 +19,9 @@
     </header>
 
     <section id="hero">
-        <!-- <img src="icons/shopping_cart.svg" class="cart-icon"> -->
-        <input type="image" src="icons/shopping_cart.svg" class="cart-button"/>
+        <form action="checkout.php">
+            <input type="image" src="icons/shopping_cart.svg" class="cart-button"/>
+        </form>
         <h2>Get Fresh Groceries Delivered to Your Doorstep</h2>
         <p>Order from a wide range of fresh fruits, vegetables, dairy products, meats, and more.</p>
     </section>
